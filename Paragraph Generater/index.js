78d29c5048ff425a088a3a540d5583d46d8ab7d1
Swapnil Shende <116657863+Swapnil-FWDeveloper@users.noreply.github.com>
2022-12-19 23:52:34 +0530

    Update index.js

--- Paragraph Generater/index.js
+++ Paragraph Generater/index.js
@@ -1,1 +1,26 @@
 
+
+
+const input=document.getElementById("input-box")
+
+// Step-1
+// let num;
+// // use num we will store our value in num 
+
+// const generatePara=()=>{
+//      num=(Number(input.value))
+//     //   console.log(num)
+// }
+// and here I have done function call in index.html on button and then we can access the value but all the value type is string so I will rap 
+// all the value in number 
+
+// step-2
+// Now we will make one paragraphh and I will give class as myPara and we will append in the parent div which is container and when button is clicked
+// at that time new para will generated
+
+let num;
+// use num we will store our value in num 
+const generatePara=()=>{
+     num=(Number(input.value))
+    //   console.log(num)
+}
